6b82f7af6c79bcd5a7b9988bc634c5b5c4bfc0d2
dashath0097 <dashathk0440@gmail.com>
2025-03-06 18:55:40 +0530

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,22 @@
+terraform {
+  required_providers {
+    spacelift = {
+      source  = "spacelift-io/spacelift"
+      version = "~> 1.0"
+    }
+  }
+}
+
+provider "spacelift" {}
+
+resource "spacelift_aws_integration" "developer_aws" {
+  for_each  = toset(var.aws_account_ids)  # Loop through each AWS account ID
+
+  name     = "AWS-${each.value}"
+  role_arn = "arn:aws:iam::${each.value}:role/Spacelift"
+  space_id = var.spacelift_space_id
+}
+
+output "integration_ids" {
+  value = { for acc_id in var.aws_account_ids : acc_id => spacelift_aws_integration.developer_aws[acc_id].id }
+}
